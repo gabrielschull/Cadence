2d8852dbe528ad9a1825e7f9e4c6944fa67ca7e2
Gabriel <gschull991@gmail.com>
2023-11-17 16:31:04 -0500

    boilerplate server for scraping, node updated to 20.9.0, puppeteer install

---
+++ src/server/server.js
@@ -0,0 +1,41 @@
+import cors from 'cors';
+import express from 'express';
+import puppeteer from 'puppeteer';
+import { supabase } from '../../supabaseClient'; // Adjust the path as per your project structure
+
+const app = express();
+const port = 3000;
+app.use(cors())
+app.use(express.json());
+
+// Example endpoint for web scraping and storing data to Supabase
+app.get('/scrape', async (req, res) => {
+    try {
+        // Use Puppeteer to scrape data
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://example.com'); // Replace with the URL you want to scrape
+        const scrapedData = await page.evaluate(() => {
+            // Scrape data and return the necessary information
+            // Example: return document.title;
+        });
+        await browser.close();
+
+        // Store the scraped data in Supabase
+        const { data, error } = await supabase
+            .from('your_table') // Replace with your table name
+            .insert([{ scraped_field: scrapedData }]); // Replace with your field names and data
+
+        if (error) throw error;
+        
+        // Send response back
+        res.json({ message: 'Scraping and data insertion successful', data });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
