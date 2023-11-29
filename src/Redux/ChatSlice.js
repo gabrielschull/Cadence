3321fb4530f384aa814c35bc72f96c1a8bff919b
Gabriel <gschull991@gmail.com>
2023-11-28 22:35:53 -0500

    redux chat implementation

---
+++ src/Redux/ChatSlice.js
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+    activeChatId: null,
+  conversationHistory: null,
+  currentDocument: null,
+  login: null,
+  openDraw: false,
+};
+
+
+
+
+export const ChatSlice = createSlice({
+  name: 'chat',
+  initialState,
+  reducers: {
+    setActiveChatId: (state, action) => {
+        state.activeChatId = action.payload
+        },
+    setCurrentDocument: (state, action) => {
+        state.currentDocument = action.payload
+        },
+    setOpenDraw: (state, action) => {    
+        state.openDraw = action.payload
+        },
+    setConversationHistory: (state, action) => {
+        state.conversationHistory = action.payload
+        }
+  },
+});
+export const {
+    setActiveChatId,
+    setCurrentDocument,
+    setOpenDraw,
+    setConversationHistory,
+} = ChatSlice.actions;
+export default ChatSlice.reducer;
--- src/Redux/RootReducer.js
+++ src/Redux/RootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './UserSlice'
 import clientsReducer from './ClientsSlice'
+import chatReducer from './ChatSlice'
 
 const rootReducer = combineReducers({
     User: userReducer,
     Clients: clientsReducer,
+    Chat: chatReducer,
 });
 
 export default rootReducer;
